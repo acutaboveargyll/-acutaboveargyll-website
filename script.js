067ce0d4372c33be06c9539d25f6edc6f7fd8d4c
acutaboveargyll <acutaboveargyll@yahoo.com>
2026-07-22 12:05:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+// A Cut Above Argyll V2
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("A Cut Above Argyll Website Loaded");
+
+    // Smooth scrolling
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+        anchor.addEventListener("click", function(e){
+
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute("href"))
+
+                .scrollIntoView({
+
+                    behavior:"smooth"
+
+                });
+
+        });
+
+    });
+
+});
